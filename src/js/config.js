df77e6e7d794735388e0e09901de1af5a752cb70
Ajaykumar Tank <>
2018-09-29 16:21:45 +0530

    appending cors url and listening on 0.0.0.0

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
 export const key='dc6110e56b6573460f5a6e7d933fda77';
-export const url = `http://food2fork.com/api/`;
+export const url = 'https://cors-anywhere.herokuapp.com/http://food2fork.com/api/';
